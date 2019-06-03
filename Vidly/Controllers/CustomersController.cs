d7dfc49304747d98df17137e1524d21dc00fdcb5
Zoran <zvulanovic@yahoo.com>
2019-06-03 18:10:46 +0200

    Inital work

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+			var customers = new List<Customer>
+			{
+					new Customer { Id=1, Name = "John Smit"},
+					new Customer { Id=2, Name = "Mary Williams"},
+			};
+			var viewModel = new IndexCustomersViewModel
+			{
+				Customers = customers
+			};
+			return View(viewModel);
+        }
+
+
+		[Route("customers/list/{id}")]
+		public ActionResult Detail(int id)
+		{
+			
+			return Content("ID="+ id);
+		}
+		
+	}
+}
